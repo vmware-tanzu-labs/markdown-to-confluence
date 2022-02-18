06e71abad3ca51cf98761c9296eb6f09da8d5c29
Paul Wiggett <pwiggett@vmware.com>
2022-02-18 11:52:40 +0000

    changed layout to include spacer and stop rendering toc if no toc exists on page

--- markdown_to_confluence/convert.py
+++ markdown_to_confluence/convert.py
@@ -56,19 +56,19 @@ class ConfluenceRenderer(mistune.Renderer):
 
     def layout(self, content):
         """Renders the final layout of the content. This includes a two-column
-        layout, with the authors and ToC on the left, and the content on the
+        layout, with the ToC on the left, spacer column in the middle and the content on the
         right.
 
         The layout looks like this:
 
-        ------------------------------------------
-        |             |                          |
-        |             |                          |
-        | Sidebar     |         Content          |
-        | (30% width) |      (800px width)       |
-        |             |                          |
-        ------------------------------------------
-        
+        -------------------------------------------
+        |             |  |                        |
+        |             |  |                        |
+        | Sidebar     |  |       Content          |
+        | (25% width) |  |    (73% width)         |
+        |             |  |                        |
+        -------------------------------------------
+
         Arguments:
             content {str} -- The HTML of the content
         """
@@ -77,19 +77,30 @@ class ConfluenceRenderer(mistune.Renderer):
             <p><ac:structured-macro ac:name="toc" ac:schema-version="1">
                 <ac:parameter ac:name="exclude">^(Authors|Table of Contents)$</ac:parameter>
             </ac:structured-macro></p>''')
-        # Ignore the TOC if we haven't processed any headers to avoid making a
-        # blank one
+        
+        if self.has_toc:
+            spacer = ''
+            #authors = self.render_authors()
+            column = textwrap.dedent('''
+                <ac:structured-macro ac:name="column" ac:schema-version="1">
+                    <ac:parameter ac:name="width">{width}</ac:parameter>
+                    <ac:rich-text-body>{content}</ac:rich-text-body>
+                </ac:structured-macro>''')
+            sidebar = column.format(width='25%', content=toc)
+            spacer = column.format(width='2%', content=spacer)
+            main_content = column.format(width='73%', content=content)
+            return sidebar + spacer + main_content
+        
+        # Ignore the TOC if none in source for better formatting
         if not self.has_toc:
-            toc = ''
-        authors = self.render_authors()
-        column = textwrap.dedent('''
-            <ac:structured-macro ac:name="column" ac:schema-version="1">
-                <ac:parameter ac:name="width">{width}</ac:parameter>
-                <ac:rich-text-body>{content}</ac:rich-text-body>
-            </ac:structured-macro>''')
-        sidebar = column.format(width='30%', content=toc + authors)
-        main_content = column.format(width='800px', content=content)
-        return sidebar + main_content
+            #authors = self.render_authors()
+            column = textwrap.dedent('''
+                <ac:structured-macro ac:name="column" ac:schema-version="1">
+                    <ac:parameter ac:name="width">{width}</ac:parameter>
+                    <ac:rich-text-body>{content}</ac:rich-text-body>
+                </ac:structured-macro>''')
+            main_content = column.format(width='100%', content=content)
+            return main_content
 
     def header(self, text, level, raw=None):
         """Processes a Markdown header.
